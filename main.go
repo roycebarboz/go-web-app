4afc120684ea2772f257ca25f0000c7bc30e4289
Royce <roycebarboz@gmail.com>
2025-07-31 03:04:18 -0400

    ci_check2

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// test ci
 func homePage(w http.ResponseWriter, r *http.Request) {
 	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
